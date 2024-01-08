405431740b51aaa9be6681d5b4c58518f2cd10fa
SeoU <tjdnfkd1225@naver.com>
2024-01-09 03:28:51 +0900

    MagazineApp 구현

---
+++ MagazineApp/Magazine.swift
@@ -0,0 +1,8 @@
+//
+//  Magazine.swift
+//  MagazineApp
+//
+//  Created by 은서우 on 2024/01/09.
+//
+
+import Foundation
---
+++ MagazineApp/MagazineTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MagazineTableViewCell.swift
+//  MagazineApp
+//
+//  Created by 은서우 on 2024/01/09.
+//
+
+import UIKit
+
+class MagazineTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
