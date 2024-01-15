84b561cbd4a3c4795c6bdc7cf58e6a3af1d3b186
SeoU <tjdnfkd1225@naver.com>
2024-01-15 16:11:36 +0900

    Model 추가, 폴더링

---
+++ MagazineApp/Model/Magazine.swift
@@ -0,0 +1,16 @@
+//
+//  Magazine.swift
+//  MagazineApp
+//
+//  Created by 은서우 on 2024/01/15.
+//
+
+import Foundation
+
+struct Magazine {
+    let title: String
+    let subtitle: String
+    let photo_image: String
+    let date: String
+    let link: String
+}
---
+++ MagazineApp/Model/Theater.swift
@@ -0,0 +1,17 @@
+//
+//  Theater.swift
+//  MagazineApp
+//
+//  Created by 은서우 on 2024/01/15.
+//
+
+import Foundation
+
+struct Theater {
+    
+    let type: String
+    let location: String
+    let latitude: Double
+    let longitude: Double
+    
+}
